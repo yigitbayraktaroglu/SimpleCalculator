76505fd061235ea5a3d6b5e79e33d32ea357d389
yigitbayraktaroglu <yigitbayraktaroglu@marun.edu.tr>
2024-02-28 00:22:36 +0300

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Yiğit Bayraktaroğlu on 28.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
