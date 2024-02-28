ff61fc2a23d1a775dba25d9ce3e1ebf94f0b5ab5
yigitbayraktaroglu <yigitbayraktaroglu@marun.edu.tr>
2024-02-29 00:03:22 +0300

    operations added

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
+    var resultNum=0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func sumClicked(_ sender: Any) {
+        if let firstNum = Int(firstNumber.text!){
+            if let secondNum = Int(secondNumber.text!){
+                resultNum = firstNum + secondNum
+                result.text = String(resultNum)
+            }
+        }
+        
+    }
+    
+    @IBAction func minusClicked(_ sender: Any) {
+        if let firstNum = Int(firstNumber.text!){
+            if let secondNum = Int(secondNumber.text!){
+                resultNum = firstNum - secondNum
+                result.text = String(resultNum)
+            }
+        }
+    }
+    
+    @IBAction func divideClicked(_ sender: Any) {
+        if let firstNum = Int(firstNumber.text!){
+            if let secondNum = Int(secondNumber.text!){
+                resultNum = firstNum / secondNum
+                result.text = String(resultNum)
+            }
+        }
+    }
+    
+    @IBAction func multiplyClicked(_ sender: Any) {
+        if let firstNum = Int(firstNumber.text!){
+            if let secondNum = Int(secondNumber.text!){
+                resultNum = firstNum * secondNum
+                result.text = String(resultNum)
+            }
+        }
+    }
+    
+    
 }
 
